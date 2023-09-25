444e6846de7f04f375d28957f59882b357be2048
Christian Henkel <christian.henkel2@de.bosch.com>
2023-09-25 16:11:45 +0200

    quicker test
    Signed-off-by: Christian Henkel <christian.henkel2@de.bosch.com>

--- test_cuda.py
+++ test_cuda.py
@@ -19,12 +19,14 @@ def test_speed():
     for device in devices:
         durations[device] = []
         for _ in range(n_tests):
-            x = torch.randn(10000, 10000, device=device)
-            y = torch.randn(10000, 10000, device=device)
+            x = torch.randn(1000, 1000, device=device)
+            y = torch.randn(1000, 1000, device=device)
             t0 = time.time()
             z = torch.matmul(x, y)
             durations[device].append(time.time() - t0)
 
     print(durations)
     for i in range(n_tests):
+        if i == 0:
+            continue  # skip first round, since it is usually slower
         assert durations["cuda"][i] < durations["cpu"][i]
